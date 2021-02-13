1ec5301b136a6998baf01f786e34bbdc147f3f54
Mary Anne Woodman <42190035+mwoodman30@users.noreply.github.com>
2021-02-13 09:24:40 -0500

    Add files via upload

--- weather/app.js
+++ weather/app.js
@@ -1,6 +1,6 @@
 const button = document.querySelector("#generate")
-const inputValue = document.querySelector(".inputValue")
-const feeling = document.querySelector('#feeling').value;
+/*const inputValue = document.querySelector(".inputValue")
+
 const name = document.querySelector("#name");
 const desc = document.querySelector("#content");
 const temp = document.querySelector("#temp");
@@ -9,6 +9,8 @@ const Kelvin = 273;
 //let d = new Date();
 //let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+
+//WEATHER FUNCTION
 button.addEventListener('click',function () {
         fetch('https://api.openweathermap.org/data/2.5/weather?zip=' +inputValue.value + '&appid=3e0ab282071ecddd139e033e5fb77b61')
             .then(response => response.json())
@@ -26,21 +28,44 @@ button.addEventListener('click',function () {
             .catch(err => alert("wrong zip code"))
     })
 
-
-//let d = new Date();
-//let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-
+// DATE AND TIME FUNCTION
 button.addEventListener('click',function () {
     fetch('http://worldclockapi.com/api/json/est/now')
     .then(response => response.json())
     .then(data => {
         const dateValue = data['currentDateTime'];
        
-        date.innerHTML = dateValue;
-        document.getElementById('feeling').innerHTML = feeling;
-    })
-})
+      date.innerHTML = dateValue;
+    })  
+})*/
 
-/*button.addEventListener('click',function(){
-    //const feeling = d
-    docum*/
+// FEELING FUNCTION
+button.addEventListener('click',function(){
+    const feeling = document.querySelector('#feeling').value;
+    document.getElementById('feeling');
+    feeling.innerHTML = feeling;
+  
+})
+/*
+// POST function to server
+async function postData(url, data) {
+    await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    });
+}
+
+//GET FUNCITON
+async function getServerData() {
+    const response = await fetch('/return');
+    const currentEntry = await response.json();
+    if (currentEntry  && currentEntry.temp) {
+        updateUI(currentEntry);
+    }
+}
+
+function updateUI(weather) {
+    console.log(weather);
+}*/
